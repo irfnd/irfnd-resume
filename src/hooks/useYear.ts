4250fc07b0405324e292821fe31dbd3ddf440ebd
Irfandi Iqbal Abimanyu <irfndiabimanyu@gmail.com>
2026-02-24 15:21:22 +0700

    fix: remove unnecessary 1s polling in useYear, compute once on mount

--- src/hooks/useYear.ts
+++ src/hooks/useYear.ts
@@ -1,18 +1,6 @@
-import * as React from 'react';
+import { useState } from 'react';
 
 export function useYear() {
-	const [year, setYear] = React.useState(new Date().getFullYear());
-
-	React.useEffect(() => {
-		const checkYear = () => {
-			const currentYear = new Date().getFullYear();
-			if (currentYear !== year) setYear(currentYear);
-		};
-
-		const interval = setInterval(checkYear, 1000);
-
-		return () => clearInterval(interval);
-	}, [year]);
-
+	const [year] = useState(() => new Date().getFullYear());
 	return year;
 }
